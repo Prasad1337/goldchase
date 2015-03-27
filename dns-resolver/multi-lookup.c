9adb8f39e8a105069a98bbfe33fc75ba92427253
Pradyumna Kamat <Prasad@Prasads-MacBook-Pro.local>
2015-03-27 15:58:40 -0700

    DNR-r init fix

---
+++ dns-resolver/multi-lookup.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "multi-lookup.h"
+
+#define MINARGS 3
+#define USAGE "<inputFilePath> <outputFilePath>"
+#define SBUFSIZE 1025
+#define INPUTFS "%1024s"
+
+int main()//int argc, char* argv[]
+{
+
+	return EXIT_SUCCESS;
+}
