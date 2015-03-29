d16eafb67eff67e0c16c0dc96010ec0f0564b286
Pradyumna <prasad1337@gmail.com>
2015-03-28 20:29:42 -0700

    near final

--- dns-resolver/multi-lookup.c
+++ dns-resolver/multi-lookup.c
@@ -1,23 +1,16 @@
+//
+// Pradyumna Kamat : pkamat (Prasad1337)
+//
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <pthread.h>
 #include <unistd.h>
-#include "util.h"
-#include "queue.h"
+#include "multi-lookup.h"
 
-#define MIN_ARGS 3
-#define USAGE "./multi-lookup <inputFilePath> <inputFilePath> ... <outputFilePath>"
-#define SBUFSIZE 1025
-#define INPUTFS "%1024s"
-
-#define MAX_INPUT_FILES 10
-#define MAX_RESOLVER_THREADS 10
-#define MIN_RESOLVER_THREADS 2
-#define MAX_NAME_LENGTH 1025
-#define MAX_IP_LENGTH INET6_ADDRSTRLEN
-#define QUEUE_SIZE 1024
 
 FILE* outputfp;
 queue q;
