2dc713a994ba8db6dd58ecd1a3f76ef92725df76
Prasad1337 <prasad1337@gmail.com>
2015-05-03 23:09:57 -0700

    lru ready

--- paging-handout/pager-lru.c
+++ paging-handout/pager-lru.c
@@ -27,73 +27,61 @@ static int pages_alloc(Pentry q[MAXPROCESSES],int proc)
 
 static void lru_page(Pentry q[MAXPROCESSES],int proc,int tick,int *evictee)
 {
-    int pg;
-    int t;
+    int pg,t;
 
-    *evictee = -1;
-    t = tick+1;  
+    *evictee=-1;
+    t=tick+1;  
 
-    for(pg = 0; pg < MAXPROCPAGES; pg++) {
-        if(!q[proc].pages[pg]) /* cant evict this */
+    for(pg=0;pg<MAXPROCPAGES;pg++)
+    {
+        if(!q[proc].pages[pg])
             continue;
 
-        if(timestamps[proc][pg] < t) {
-            t = timestamps[proc][pg];
-            *evictee = pg;
+        if(timestamps[proc][pg]<t)
+        {
+            t=timestamps[proc][pg];
+            *evictee=pg;
 
-            if(t <= 1) /* cant do any better than that! */
+            if(t<=1)
                 break;
         }
-    }           
+    }
 
-    if(*evictee < 0) {
-        printf("page for process %d w/ %u active pages not found with age < %u\n", proc, (unsigned int) pages_alloc(q, proc), tick);
+    if(*evictee<0)
+    {
+        printf("page for process %d w/ %u active pages not found with age < %u\n",proc,(unsigned int)pages_alloc(q,proc),tick);
         fflush(stdout);
-        //endit();
     }
 }
     
-static void lru_pageit(Pentry q[MAXPROCESSES], int tick)
+static void lru_pageit(Pentry q[MAXPROCESSES],int tick)
 {
-    int proc, pg, evicted;
+    int proc,pg,evicted;
     
-    for(proc = 0; proc < MAXPROCESSES; proc++) {
-        if(!q[proc].active) /* done if its not active */
-            continue;
-
-        pg = q[proc].pc/PAGESIZE;
-        /* note this time for future eviction decisions */
-        timestamps[proc][pg] = tick; 
+    for(proc=0;proc<MAXPROCESSES;proc++)
+    {
+        pg=q[proc].pc/PAGESIZE;
+        timestamps[proc][pg]=tick; 
         
-        /* done if the page is already in memory */
-        if(q[proc].pages[pg]) 
+        if(pagein(proc,pg))
             continue;
 
-        /* the page is not in memory.
-         * if pagein give 1 the page is either 
-         * on its way already or we just got it
-         * started, so we are done with this process
-         */
-        if(pagein(proc, pg) )
+        if(pages_alloc(q,proc)<1) 
             continue;
 
-        /* there are no free physical pages */
-        if(pages_alloc(q, proc) < 1) 
-            continue; /* must have at least one page to evict */
-
-        lru_page(q, proc, tick, &evicted);
-        pageout(proc, evicted);
+        lru_page(q,proc,tick,&evicted);
+        pageout(proc,evicted);
     }
 }
 
-void exit_fn()
+void term()
 {
-    printf("final tick value was %d\n", tick);  
+    printf("final tick value was %d\n",tick);  
 }
 
 void pageit(Pentry q[MAXPROCESSES])
 {
-    if(tick < 1)
+    if(tick<1)
     {
         int proc,pg;
 
@@ -103,16 +91,10 @@ void pageit(Pentry q[MAXPROCESSES])
                 timestamps[proc][pg]=0; 
         }
 
-        tick = 1;
-
-        if(atexit(exit_fn) != 0)
-        {
-            perror(NULL);
-            exit(1);
-        }
-    }   
+        tick=1;
+        atexit(term);
+    }
     
     lru_pageit(q,tick);
     tick++;
-} 
-
+}
