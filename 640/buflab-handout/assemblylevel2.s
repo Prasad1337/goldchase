2ecd59113d7e8e99df0161c255ad6c5cbeb15641
Prasad <pkamat@csuchico.edu>
2016-01-13 12:05:48 -0800

    goldchase initial repo commit

--- 640/buflab-handout/assemblylevel2.s
+++
@@ -1,3 +0,0 @@
-movl $0x1b39bd38, 0x0804d100 
-pushl $0x08048c1d           
-ret
--- 640/buflab-handout/assemblylevel4.s
+++
@@ -1,4 +0,0 @@
-lea 0x28 (%esp)   , %ebp # restore ebp register contents
-movl  $0x1b39bd38 , %eax # returns the cookie value
-pushl $0x08048dba        # return address pointing instruction after getbufn() call in testn()
-ret
